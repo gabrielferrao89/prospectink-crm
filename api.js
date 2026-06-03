1b3b9ee55dac8f27f904bb0f98464fc06f9fcbab
gabrielferrao89 <gabriel.ferrao@blindowlch.com>
2026-06-03 14:37:45 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,37 +1,20 @@
-const https = require('https');
-
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwWPiy97_579ecssAmznzk8RkL3SwhEd04NbySLA_W7CHf-mSE4-Y4xlwDEUKGo73FL/exec';
-
 exports.handler = async function(event) {
-  const params = new URLSearchParams(event.queryStringParameters).toString();
-  const url = APPS_SCRIPT_URL + '?' + params;
+  var SCRIPT = 'https://script.google.com/macros/s/AKfycbwWPiy97_579ecssAmznzk8RkL3SwhEd04NbySLA_W7CHf-mSE4-Y4xlwDEUKGo73FL/exec';
+  var params = new URLSearchParams(event.queryStringParameters || {}).toString();
+  var url = SCRIPT + (params ? '?' + params : '');
 
   try {
-    const data = await new Promise((resolve, reject) => {
-      let result = '';
-      const makeRequest = (requestUrl) => {
-        https.get(requestUrl, (res) => {
-          if (res.statusCode === 302 || res.statusCode === 301) {
-            makeRequest(res.headers.location);
-            return;
-          }
-          res.on('data', chunk => result += chunk);
-          res.on('end', () => resolve(result));
-          res.on('error', reject);
-        }).on('error', reject);
-      };
-      makeRequest(url);
-    });
-
+    var response = await fetch(url, { redirect: 'follow' });
+    var text = await response.text();
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       },
-      body: data
+      body: text
     };
-  } catch (err) {
+  } catch(err) {
     return {
       statusCode: 500,
       body: JSON.stringify({ error: err.message })
